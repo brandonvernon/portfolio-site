73f59bc7f4326c880fa130d83ce39092a80218c9
Brandon Vernon <brandontvernon@gmail.com>
2018-10-18 02:17:50 -0500

    Updates

--- Portfolio Site/js/app.js
+++ Portfolio Site/js/app.js
@@ -1,46 +1,58 @@
-let contact = document.querySelector('.navContact');
-let portfolio = document.querySelector('.navPortfolio');
-let aboutMe = document.querySelector('.navAboutMe');
+let overlay = document.querySelector('.modal-overlay');
+let aboutMe = document.querySelector('.nav-aboutme');
+let portfolio = document.querySelector('.nav-portfolio');
+let contact = document.querySelector('.nav-contact');
+let aboutMeModal = document.querySelector('.aboutme-modal');
+let portfolioModal = document.querySelector('.portfolio-modal');
+let contactModal = document.querySelector('.contact-modal');
+let header = document.querySelector('.header-wrapper');
 
-let contactModal = document.querySelector('.contactModal');
-let portfolioModal = document.querySelector('.portfolioModal');
-let aboutMeModal = document.querySelector('.aboutMeModal');
-
-let name = document.querySelector('.name');
-let role = document.querySelector('.role');
-
-
-function clear() {
-  contact.addEventListener("click", function(event) {
-    name.classList.add('clear');
-    role.classList.add('clear');
-    unclear();
+function init() {
+  aboutMe.addEventListener("click", function(event) {
+    header.classList.add('hide');
+    aboutMeModal.style.display = "block";
+    overlay.style.display = "block";
+    close();
   })
   portfolio.addEventListener("click", function(event) {
-    name.classList.add('clear');
-    role.classList.add('clear');
-    unclear();
+    header.classList.add('hide');
+    portfolioModal.style.display = "flex";
+    overlay.style.display = "block";
+    close();
   })
-  aboutMe.addEventListener("click", function(event) {
-    name.classList.add('clear');
-    role.classList.add('clear');
-    unclear();
-  })
-}
-
-function unclear() {
-  contactModal.addEventListener("click", function(event) {
-      name.classList.remove('clear');
-      role.classList.remove('clear');
-  })
-  portfolioModal.addEventListener("click", function(event) {
-      name.classList.remove('clear');
-      role.classList.remove('clear');
-  })
-  aboutMeModal.addEventListener("click", function(event) {
-      name.classList.remove('clear');
-      role.classList.remove('clear');
+  contact.addEventListener("click", function(event) {
+    header.classList.add('hide');
+    contactModal.style.display = "block";
+    overlay.style.display = "block";
+    close();
   })
 }
 
-clear();
+function close() {
+  overlay.addEventListener("click", function(event) {
+      header.classList.remove('hide');
+      aboutMeModal.style.display = "none";
+      portfolioModal.style.display = "none";
+      contactModal.style.display = "none";
+      overlay.style.display = "none";
+  })
+}
+
+function check() {
+  window.addEventListener("resize", noModals);
+}
+
+function noModals(){
+  let width = window.innerWidth;
+  console.log(width);
+  if (width <= "768"){
+    header.classList.remove('hide');
+    aboutMeModal.style.display = "none";
+    portfolioModal.style.display = "none";
+    contactModal.style.display = "none";
+    overlay.style.display = "none";
+  }
+}
+
+init();
+check();
